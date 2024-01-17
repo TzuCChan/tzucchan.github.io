0774d03c8207f65062eb8a96e1c72bc9898bc7aa
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-01-17 17:22:38 -0500

    contact jsx

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3fjfb17', 'template_ky1ucaz', form.current, 'user_641J0AWGxx4qcKi835yDq')
+
+    e.target.reset()
+  };
+
+  return (
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>tzuchiangchan@gmail.com</h5>
+            <a href="mailto:tzuchiangchan@gmail.com" target="_blank">Send an email</a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className='contact__option-icon'/>
+            <h4>Call</h4>
+            <h5>646-651-2532</h5>
+            <a href="tel:+6466512532" target="_blank">Call me</a>
+          </article>
+        </div>
+    
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Full Name' required />
+          <input type="email" name='email' placeholder='Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
+  )
+}
+
+export default Contact
