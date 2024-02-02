e619caf21209e95900edec93ec610e008a853173
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-02-02 16:16:39 -0500

    fsd -> fed header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello! My name is</h5>
         <h1>Leon</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Front End Developer</h5>
         <CTA />
         <HeaderSocial />
 
